f1f8841b6e5e3ab071ddd7c6c5deeb68be47d780
SofiaErnScheidt <162714170+SofiaErnScheidt@users.noreply.github.com>
2024-08-02 11:40:27 -0300

    Add files via upload

---
+++ sofia/sofia.js
@@ -0,0 +1,35 @@
+let primeiroNome = 'Sofia'
+let ultimoNome = 'Ern'
+let nomeCompleto = primeiroNome + ' ' + ultimoNome
+
+let numero1 = 10
+let numero2 = 20
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1/ numero2
+console.log(soma, subtracao, multiplicacao, divisao )
+
+let estaChovendo = true
+if(estaChovendo){
+    console.log('Está chovendo')
+}else{
+    console.log('Não está chovendo')
+}
+
+let resposta = null
+let nota 
+console.log(resposta, nota)
+
+let idUnico = Symbol()
+let produto = { 
+    [idUnico]: 123,
+    nome: "camisa"
+}
+console.log (produto[idUnico], produto.nome)
+
+let numeroGrande = BigInt('123456789123456789123456789123456789123456789')
+let numero = 123456789123456789123456789123456789123456789
+let numero3 = numeroGrande
+console.log(numeroGrande, numero, "\n", numero3)
